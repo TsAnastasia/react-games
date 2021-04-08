1c0cef2728a8567f5d7ae5972213dfe8c2c940e2
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-04-08 22:35:15 +0300

    feat: add info in header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,26 @@
 4. Add switch to sort moves in history.
 5. Highlight cells on win.
 6. Show message when noone win. */
-import React from 'react';
-import CrossZero from './CrossZero/CrossZero';
+import React from "react";
+import CrossZero from "./CrossZero/CrossZero";
 
-const App = () =>{
+const App = () => {
   return (
-    <>
-      <header></header>
-      <main>
+    <div className="page">
+      <header className="page__header">
+        <h1 class="page__title">Croos-Zero</h1>
+      </header>
+      <main className="page__content">
         <CrossZero />
       </main>
-      <footer></footer>
-    </>
-  )
-}
+      <footer className="page__footer">
+        <p className="page__footer-copyright">
+          &copy; {`${new Date().getFullYear()}`} {/* React Games */},
+          Анастасия&nbsp;Циберная
+        </p>
+      </footer>
+    </div>
+  );
+};
 
 export default App;
