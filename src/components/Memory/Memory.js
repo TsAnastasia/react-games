dce0284bb5da3e3c73ac8310700f45c2eedbd1cb
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-04-12 21:05:10 +0300

    feat: compare cards

--- src/components/Memory/Memory.js
+++ src/components/Memory/Memory.js
@@ -5,33 +5,74 @@ const Card = ({ card, onClick }) => {
   // const [isHidden, setIsHiddeen] = React.useState(false);
 
   const toggleClick = () => {
-    
+    if (card.isOpen) return;
     onClick(card);
   }
 
   return (
     <li className="memory__item card" onClick={toggleClick}>
-      <div className="card__area"></div>
+      <div className="card__area">{`Memory`}</div>
       <div className={`card__image ${card.isHidden && "card__image_hidden"}`}>
-        <p>simvol</p>
+        <p>{card.value}</p>
+        {/* {`${card.value} O=${card.isOpen} h=${card.isHidden}`} */}
       </div>
     </li>
   );
 };
 
 const Memory = () => {
+  const timeDelay = 1000;
+  const [rotateCards, setRotateCards] = React.useState([])
   const values = [1, 4, 3, 5, 3, 1, 2, 6, 5, 4, 6, 2];
   const [cards, setCards] = React.useState(
     values.map((value, index) => {
-      return { value, id: index, isHidden: true };
+      return { value, id: index, isHidden: true, isOpen: false };
     })
   );
 
   const toggleCardClick = (card) => {
+    setRotateCards(
+      rotateCards.concat([
+        {
+          id: card.id,
+          value: card.value,
+        },
+      ])
+    );
     setCards(cards.map(item => 
       item.id === card.id ? {...item, isHidden: !item.isHidden} : item
     ));
   }
+
+  React.useEffect(()=> {    
+    if (rotateCards.length === 2) {
+      console.log()
+      if (rotateCards[0].value === rotateCards[1].value) {
+        setCards(
+          cards.map((item) =>
+            item.id === rotateCards[0].id || item.id === rotateCards[1].id
+              ? { ...item, isOpen: true }
+              : item
+          )
+        );
+      } else {
+        setTimeout( () =>
+          setCards(
+            cards.map((item) =>
+              item.id === rotateCards[0].id || item.id === rotateCards[1].id
+                ? { ...item, isHidden: true }
+                : item
+            )
+          ),
+          timeDelay
+        );
+      }
+
+      setRotateCards([]);
+    }
+
+  }, [cards])
+
 
   return (
     <section className="game memory">
