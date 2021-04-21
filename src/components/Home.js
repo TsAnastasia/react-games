05f8e3984d169c1698764881be8d1d238c19fab7
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-04-21 21:57:25 +0300

    feat: add content on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TranslationAppContext } from "../contexts/translation/TranslationAppContext";
+import { BASE_URL } from '../utils/constants';
+
 const Home = () => {
+  const translation = React.useContext(TranslationAppContext);
+
   return (
-    <section>
-      <nav></nav>
+    <section className="page-home">
+      <p className="page-home__description">{translation.projectDescription}</p>
+      <nav className="page-home__nav">
+        <ul className="page-home__games">
+          <h2 className="page-home__games-title">{translation.projectGamesTitle}</h2>
+          <li className="page-home__games-item">
+            <Link className="page-home__link app__link" to={`${BASE_URL}/cross-zeros`}>
+              {translation.crossZeros}
+            </Link>
+          </li>
+          <li className="page-home__games-item">
+            <Link className="page-home__link app__link" to={`${BASE_URL}/memory`}>
+              {translation.memory}
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 };
